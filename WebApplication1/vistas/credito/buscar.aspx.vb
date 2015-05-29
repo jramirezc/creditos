9ee03c2da1cf0e6d726f37876b269a1cf4660648
Ericzzon <eriksson13_89@hotmail.com>
2015-05-28 22:27:22 -0600

    vacio

--- WebApplication1/vistas/credito/buscar.aspx.vb
+++
@@ -1,5 +0,0 @@
-﻿
-Partial Class vistas_credito_buscar
-    Inherits System.Web.UI.Page
-
-End Class
--- vistas/credito/crear.aspx.vb
+++
@@ -1,5 +0,0 @@
-﻿
-Partial Class vistas_credito_crear
-    Inherits System.Web.UI.Page
-
-End Class
