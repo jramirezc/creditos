b5356521c54a7fae23ab54a8a8c9fbc3f32c7db3
chayax <uchayaxtsc@gmail.com>
2015-05-29 04:15:22 -0600

    actualizar
    actualizar

--- WebApplication1/controles/preuba.vb
+++ WebApplication1/controles/preuba.vb
@@ -5,6 +5,7 @@ Imports System.Web
 Imports System.IO
 Module preuba
     Public MostrarTodo As New DataTable
+    Public Buscar As New DataTable
 
     Public Sub procMostrarTodo(ByVal tabla As String) '(ByVal Id As SqlString, ByVal Nom As SqlString)
         MostrarTodo = New DataTable
@@ -23,6 +24,76 @@ Module preuba
 
 
         objAdapter.Fill(MostrarTodo)
+        conectarce()
+
+    End Sub
+    Public Sub procBuscar(ByVal tabla As String, ByVal id As SqlString) '(ByVal Id As SqlString, ByVal Nom As SqlString)
+        Buscar = New DataTable
+        Dim comandoIG As New SqlCommand()
+        Dim paramId As New SqlParameter("@ID", SqlDbType.Int)
+        ' Dim paramNom As New SqlParameter("@NOM", SqlDbType.NVarChar)
+        paramId.Value = CInt(id)
+        ' paramNom.Value = Nom
+        comandoIG.Parameters.Add(paramId)
+        ' comandoIG.Parameters.Add(paramNom)
+        comandoIG.CommandText = "proc" & tabla & "Buscar @ID"
+        conectarce()
+        comandoIG.Connection = cnn
+
+        Dim objAdapter As New SqlDataAdapter(comandoIG)
+
+
+        objAdapter.Fill(Buscar)
+        conectarce()
+
+    End Sub
+    Public Sub procActualizar(ByVal tabla As String, ByVal id As SqlString, ByVal nom As SqlString, ByVal estado As SqlString) '(ByVal Id As SqlString, ByVal Nom As SqlString)
+        Buscar = New DataTable
+        Dim comandoIG As New SqlCommand()
+        Dim paramId As New SqlParameter("@ID", SqlDbType.Int)
+        Dim paramEst As New SqlParameter("@EST", SqlDbType.Int)
+        Dim paramNom As New SqlParameter("@NOM", SqlDbType.NVarChar)
+        paramId.Value = CInt(id)
+        paramNom.Value = nom
+        paramEst.Value = CInt(estado)
+        ' paramNom.Value = Nom
+        comandoIG.Parameters.Add(paramId)
+        comandoIG.Parameters.Add(paramNom)
+        comandoIG.Parameters.Add(paramEst)
+
+        comandoIG.CommandText = "proc" & tabla & "Actualizar @ID, @NOM, @EST"
+        conectarce()
+        comandoIG.Connection = cnn
+        comandoIG.ExecuteNonQuery()
+        'Dim objAdapter As New SqlDataAdapter(comandoIG)
+
+
+        'objAdapter.Fill(Buscar)
+        conectarce()
+
+    End Sub
+    Public Sub procCrear(ByVal tabla As String, ByVal id As SqlString, ByVal nom As SqlString, ByVal estado As SqlString) '(ByVal Id As SqlString, ByVal Nom As SqlString)
+        Buscar = New DataTable
+        Dim comandoIG As New SqlCommand()
+        Dim paramId As New SqlParameter("@ID", SqlDbType.Int)
+        Dim paramEst As New SqlParameter("@EST", SqlDbType.Int)
+        Dim paramNom As New SqlParameter("@NOM", SqlDbType.NVarChar)
+        paramId.Value = CInt(id)
+        paramNom.Value = nom
+        paramEst.Value = CInt(estado)
+        ' paramNom.Value = Nom
+        comandoIG.Parameters.Add(paramId)
+        comandoIG.Parameters.Add(paramNom)
+        comandoIG.Parameters.Add(paramEst)
+
+        comandoIG.CommandText = "proc" & tabla & "Actualizar @ID, @NOM, @EST"
+        conectarce()
+        comandoIG.Connection = cnn
+        comandoIG.ExecuteNonQuery()
+        'Dim objAdapter As New SqlDataAdapter(comandoIG)
+
+
+        'objAdapter.Fill(Buscar)
         conectarce()
 
     End Sub
---
+++ WebApplication1/vistas/parametrizar/index.aspx.designer.vb
@@ -0,0 +1,105 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class prueba
+
+    '''<summary>
+    '''Control GridView1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents GridView1 As Global.System.Web.UI.WebControls.GridView
+
+    '''<summary>
+    '''Control Label1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Label1 As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control SqlDataSource1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents SqlDataSource1 As Global.System.Web.UI.WebControls.SqlDataSource
+
+    '''<summary>
+    '''Control LinkButton2.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents LinkButton2 As Global.System.Web.UI.WebControls.LinkButton
+
+    '''<summary>
+    '''Control LinkButton3.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents LinkButton3 As Global.System.Web.UI.WebControls.LinkButton
+
+    '''<summary>
+    '''Control LinkButton4.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents LinkButton4 As Global.System.Web.UI.WebControls.LinkButton
+
+    '''<summary>
+    '''Control LinkButton5.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents LinkButton5 As Global.System.Web.UI.WebControls.LinkButton
+
+    '''<summary>
+    '''Control LinkButton6.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents LinkButton6 As Global.System.Web.UI.WebControls.LinkButton
+
+    '''<summary>
+    '''Control LinkButton7.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents LinkButton7 As Global.System.Web.UI.WebControls.LinkButton
+
+    '''<summary>
+    '''Control LinkButton8.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents LinkButton8 As Global.System.Web.UI.WebControls.LinkButton
+End Class
